984e4f1f6845af8b1c2210dcda7465e733390cec
muhittin.kaya <muhittin.kaya@kobil.com>
2021-09-14 11:36:35 +0300

    Initial commit

---
+++ src/main/java/kaya/springexample/springpetclinicexample/SpringPetclinicExampleApplication.java
@@ -0,0 +1,13 @@
+package kaya.springexample.springpetclinicexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetclinicExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaya/springexample/springpetclinicexample/SpringPetclinicExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kaya.springexample.springpetclinicexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
