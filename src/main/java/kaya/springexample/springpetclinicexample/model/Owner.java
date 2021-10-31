af8b83bfb6bfa247037d80b7a9503110a93a6f8b
muhittin.kaya <muhittin.kaya@kobil.com>
2021-10-31 17:35:41 +0300

    Added POJOs under the model package.

---
+++ src/main/java/kaya/springexample/springpetclinicexample/model/Owner.java
@@ -0,0 +1,4 @@
+package kaya.springexample.springpetclinicexample.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/kaya/springexample/springpetclinicexample/model/Vet.java
@@ -0,0 +1,4 @@
+package kaya.springexample.springpetclinicexample.model;
+
+public class Vet extends Person{
+}
