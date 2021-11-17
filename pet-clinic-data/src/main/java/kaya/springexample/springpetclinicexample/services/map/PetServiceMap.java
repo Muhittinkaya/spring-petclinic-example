f237e231fe708fc9df7f33c10884ac523a99cc4d
muhittin.kaya <muhittin.kaya@kobil.com>
2021-11-17 12:07:46 +0300

    AbstractMapService added and other services added

---
+++ pet-clinic-data/src/main/java/kaya/springexample/springpetclinicexample/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package kaya.springexample.springpetclinicexample.services.map;
+
+import kaya.springexample.springpetclinicexample.model.Pet;
+import kaya.springexample.springpetclinicexample.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.finById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
